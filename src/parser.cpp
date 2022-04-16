0daa1455eced05315e1ad2b30b8796e775b81354
strah19 <m.strahinja@yahoo.com>
2022-04-16 15:10:25 -0500

    able to parse expressions.

--- src/parser.cpp
+++ src/parser.cpp
@@ -20,10 +20,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Parser::Parser(Vec<Token>* tokens) : tokens(tokens) { }
+Parser::Parser(Vec<Token>* tokens) : tokens(tokens) { 
+    root =  new Ast_TranslationUnit;
+    current = 0;
+}
+
+Parser::~Parser() {
+    delete root;
+}
 
 void Parser::run() {
-
+    while (!is_end()) {
+        root->expression = expression();
+        match(Tok::T_SEMI);
+    }
 }
 
 Token* Parser::peek() {
@@ -34,14 +44,10 @@ Token* Parser::advance() {
     return ((!is_end()) ? &tokens->array()[current++] : &tokens->array()[current]);
 }
 
-Ast* Parser::default_ast(Ast* ast) {
-    ast->line = peek()->line;
-    return ast;
-}
-
 void Parser::match(int type) {
-    if (peek()->type == type) 
+    if (peek()->type == type) {
         advance();
+    }
     else {
         begin_custom_error();
 
@@ -56,6 +62,171 @@ void Parser::match(int type) {
     }
 }
 
+bool Parser::check(int type) {
+    if (peek()->type == type) {
+        advance();
+        return true;
+    }
+    return false;
+}
+
 bool Parser::is_end() {
     return (current == tokens->size() - 1);
+}
+
+Ast_Expression* Parser::expression() {
+    return equality();
+}
+
+Ast_Expression* Parser::equality() {
+    auto expr = comparison();
+
+    while (check(Tok::T_COMPARE_EQUAL) || check(Tok::T_NOT_EQUAL)) {
+        auto tok = tokens->array()[current - 1];
+        auto right = comparison();
+        expr = new Ast_BinaryExpression(expr, token_to_ast(&tok), right);
+    }
+
+    return expr;
+}
+
+Ast_Expression* Parser::comparison() {
+    auto expr = term();
+
+    while (check(Tok::T_LTE) || check(Tok::T_GTE) || check(Tok::T_LARROW) || check(Tok::T_RARROW)) {
+        auto tok = tokens->array()[current - 1];
+        auto right = term();
+        expr = new Ast_BinaryExpression(expr, token_to_ast(&tok), right);
+    }
+
+    return expr;
+}
+
+Ast_Expression* Parser::term() {
+    auto expr = factor();
+
+    while (check(Tok::T_PLUS) || check(Tok::T_MINUS)) {
+        auto tok = tokens->array()[current - 1];
+        auto right = factor();
+        expr = new Ast_BinaryExpression(expr, token_to_ast(&tok), right);
+    }
+
+    return expr;
+}
+
+Ast_Expression* Parser::factor() {
+    auto expr = unary();
+    
+    while (check(Tok::T_SLASH) || check(Tok::T_STAR)) {
+        auto tok = tokens->array()[current - 1];
+        auto right = unary();
+        expr = new Ast_BinaryExpression(expr, token_to_ast(&tok), right);
+    }
+
+    return expr;
+}
+
+Ast_Expression* Parser::unary() {
+    if (check(Tok::T_MINUS)) {
+        Ast_Expression* right = unary();
+        return new Ast_UnaryExpression(right, AST_UNARY_MINUS);
+    }
+
+    return primary();
+}
+
+Ast_Expression* Parser::primary() {
+    auto prime = new Ast_PrimaryExpression();
+
+    switch (peek()->type) {
+    case Tok::T_INT_CONST: {
+        prime->int_const = peek()->int_const;
+        match(Tok::T_INT_CONST);
+        break;
+    }
+    case Tok::T_IDENTIFIER: {
+        prime->ident = identifier();
+        break;
+    }
+    case Tok::T_LPAR: {
+        match(Tok::T_LPAR);
+        auto expr = expression();
+        match(Tok::T_RPAR);
+        prime->nested = expr;
+        break;
+    }
+    }
+
+    return prime;
+}
+
+Ast_Identifier* Parser::identifier() {
+    auto id = new Ast_Identifier;
+    id->id = advance()->identifier;
+    return id;
+}
+
+int Parser::token_to_ast(Token* token) {
+    switch (token->type) {
+    case Tok::T_COMPARE_EQUAL: return AST_OPERATOR_COMPARITIVE_EQUAL;
+    case Tok::T_NOT_EQUAL: return AST_OPERATOR_COMPARITIVE_NOT_EQUAL;
+    case Tok::T_LTE: return AST_OPERATOR_LTE;
+    case Tok::T_GTE: return AST_OPERATOR_GTE;
+    case Tok::T_LARROW: return AST_OPERATOR_LT;
+    case Tok::T_RARROW: return AST_OPERATOR_GT;
+    case Tok::T_PLUS: return AST_OPERATOR_ADD;
+    case Tok::T_MINUS: return AST_OPERATOR_SUB;
+    case Tok::T_STAR: return AST_OPERATOR_MULTIPLICATIVE;
+    case Tok::T_SLASH: return AST_OPERATOR_DIVISION;
+    }
+    return -1;
+}
+
+void Parser::visualize() {
+    if (root) {
+        if (root->expression)
+            visualize_expression(root->expression);
+    }
+}
+
+void Parser::visualize_expression(Ast_Expression* expr, int indent) {
+    if (expr) {
+        switch (expr->type) {
+        case AST_UNARY: {
+            auto u = AST_CAST(Ast_UnaryExpression, expr);
+            ident(indent);
+            printf("unary: %d\n", u->op);
+            if (u->next) {
+                visualize_expression(u->next, indent + 1);
+            }
+            break;
+        }
+        case AST_PRIMARY: {
+            auto p = AST_CAST(Ast_PrimaryExpression, expr);
+            ident(indent);
+            if (p->nested) {
+                printf("nested: \n");
+                visualize_expression(p->nested, indent + 1);
+            }
+            else
+                printf("primary: %f\n", p->int_const);
+            break;
+        }
+        case AST_BINARY: {
+            auto b = AST_CAST(Ast_BinaryExpression, expr);
+            ident(indent);
+            printf("binary: %d\n", b->op);
+            visualize_expression(b->left, indent + 1);
+            visualize_expression(b->right, indent + 1);
+            break;
+        }
+        }
+    }
+}
+
+void Parser::ident(int indent) {
+    while (indent > 0) {
+        printf("\t");
+        indent--;
+    }
 }
